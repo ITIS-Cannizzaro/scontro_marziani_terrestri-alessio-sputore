e18be04c18c7cacec2e63fe6db47225809fa404c
alessio-sputore <123164955+alessio-sputore@users.noreply.github.com>
2023-03-10 11:18:40 +0100

    Add files via upload

---
+++ Marziani.java
@@ -0,0 +1,8 @@
+package Gioco;
+
+public class Marziani extends Marziano{
+	Marziani( String matricola, int autonomia, int AI, int tecnologia){
+		super (matricola, autonomia,AI, tecnologia);
+	}
+
+}
---
+++ Terrestri.java
@@ -0,0 +1,8 @@
+package Gioco;
+
+public class Terrestri extends Terrestre{
+	Terrestri ( String nome, int vita, int velocita, int armamento){
+		super(nome, vita, velocita, armamento);
+	}
+
+}
